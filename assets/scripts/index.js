bb359d72f3f9dbf2abc0fad66b06d213c15ffb4b
ravenusmc <mcuddy77@gmail.com>
2017-11-26 16:07:33 -0500

    working on adding treehouse badges

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,6 @@
+//This file will handle all of the JS code for my personal page.
+
+//This function will get the data from the API and then return it. 
 function UserAction() {
     //Setting up to get the API request
     let xhr = new XMLHttpRequest();
@@ -11,18 +14,42 @@ function UserAction() {
     return response_parsed
 }
 
-response_parsed = UserAction()
+function displayInfo(response_parsed){
+  console.log(response_parsed.badges);
+  response_length = response_parsed.badges.length;
+  for (let i = 0; i < response_length; i++) {
+    //Getting the parent element that will be a container. 
+    var parent = document.getElementById('badge_container');
+    // create a new h3 element 
+    var newDiv = document.createElement("h3");
+    // creating a new img element 
+    var image = document.createElement("img");
+    //adding content to the h3 element
+    var newContent = document.createTextNode(response_parsed.badges[i].name); 
+    // add the text node to the newly created h3 element
+    newDiv.appendChild(newContent);  
+    //getting the id of where to insert the new h3 elements
+    var currentDiv = document.getElementById("badge_name");
+    //attaching the elements to the page
+    parent.insertBefore(newDiv, currentDiv);
+  }
+  
+}
 
-console.log(response_parsed.badges)
-
-
-
-
-
-
-
-// window.onload = function(){
-//   getImage();
-// }
-
+// console.log(response_parsed.badges[i].icon_url);
 // document.getElementById("dailyImage").setAttribute("src", image.url);
+
+function main() {
+  response_parsed = UserAction();
+  // console.log(response_parsed.badges)
+  displayInfo(response_parsed);
+}
+
+
+
+
+
+window.onload = function(){
+  main();
+}
+
