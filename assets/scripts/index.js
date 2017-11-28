2731cd7998e546366105e5d0b85e4d5c618ed822
ravenusmc <mcuddy77@gmail.com>
2017-11-27 21:21:48 -0500

    working on adding badge image

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,27 +15,50 @@ function UserAction() {
 }
 
 function displayInfo(response_parsed){
-  console.log(response_parsed.badges);
+  // console.log(response_parsed.badges);
   response_length = response_parsed.badges.length;
   for (let i = 0; i < response_length; i++) {
-    //Getting the parent element that will be a container. 
-    var parent = document.getElementById('badge_container');
+
+    //creating div element 
+    var main_parent = document.getElementById('badge_main_container'); //added
+    var container = document.createElement('div'); //Added
+    var gone = document.createElement('badge_container');//added
+    main_parent.insertBefore(container, gone); // added 
+
     // create a new h3 element 
     var newDiv = document.createElement("h3");
     // creating a new img element 
     var image = document.createElement("img");
-    //adding content to the h3 element
-    var newContent = document.createTextNode(response_parsed.badges[i].name); 
-    // add the text node to the newly created h3 element
-    newDiv.appendChild(newContent);  
-    //getting the id of where to insert the new h3 elements
-    var currentDiv = document.getElementById("badge_name");
-    //attaching the elements to the page
-    parent.insertBefore(newDiv, currentDiv);
+
+
+
+///////WOrks ///
+    //Getting the parent element that will be a container. 
+    //var parent = document.getElementById('badge_container');
+
+    // // create a new h3 element 
+    // var newDiv = document.createElement("h3");
+    // // creating a new img element 
+    // var image = document.createElement("img");
+    
 
     //image code
-    image.setAttribute("src", response_parsed.badges[i].icon_url)
-    var imageTag = document.getElementById('badge_image')
+    //Setting the src and class attributes for the image.
+    image.setAttribute("src", response_parsed.badges[i].icon_url);
+    image.setAttribute('class', 'image_area')
+    var imageTag = document.getElementById('badge_image');
+    //Inserting the image to the DOM
+    parent.insertBefore(image, imageTag);
+
+    //Code for the badge title
+    //adding content to the h3 element
+    // var newContent = document.createTextNode(response_parsed.badges[i].name); 
+    // // add the text node to the newly created h3 element
+    // newDiv.appendChild(newContent);  
+    // //getting the id of where to insert the new h3 elements
+    // var currentDiv = document.getElementById("badge_name");
+    // //attaching the elements to the page
+    // parent.insertBefore(newDiv, currentDiv);
   }
   
 }
@@ -45,12 +68,8 @@ function displayInfo(response_parsed){
 
 function main() {
   response_parsed = UserAction();
-  // console.log(response_parsed.badges)
   displayInfo(response_parsed);
 }
-
-
-
 
 
 window.onload = function(){
